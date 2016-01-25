4c362f2525effa8391aea4d32568862c88cfacf0
David Lee <leetdavid@outlook.com>
2016-01-25 11:55:31 +0800

    Updated GameOverMenu

--- src/isom3320/StateGameOver.java
+++ src/isom3320/StateGameOver.java
@@ -38,14 +38,19 @@ public class StateGameOver extends State {
 		GameObject.R.textFont(Main.font);
 		GameObject.R.textAlign(PConstants.CENTER);
 		GameObject.R.textSize(50);
-		GameObject.R.text("Game Over", Main.WIDTH/2, Main.HEIGHT/8);
+		GameObject.R.text("Game Over", Main.WIDTH*0.5F, Main.HEIGHT/8);
+		
+		GameObject.R.textSize(60);
+		GameObject.R.text(yourScore, Main.WIDTH/4 + 100, Main.HEIGHT/4 + 150);
+		
 		GameObject.R.textSize(20);
+		GameObject.R.text("Your Score:", Main.WIDTH/4 + 100, Main.HEIGHT/4 + 150 - 60);
 		
 		float yval = Main.HEIGHT * 0.25F;
 		
 		GameObject.R.textSize(20);
 		GameObject.R.textAlign(PConstants.CENTER);
-		GameObject.R.text("P r e s s   S p a c e   t o   p l a y   a g a i n!", Main.WIDTH/2, yval);
+		GameObject.R.text("P r e s s   S p a c e   t o   p l a y   a g a i n!", Main.WIDTH*0.5F, yval);
 		
 		yval += 40;
 		
@@ -54,15 +59,17 @@ public class StateGameOver extends State {
 			int score = Main.highscores.get(i);
 			if(yourScore == score) {
 				GameObject.R.textSize(40);
+				GameObject.R.fill(255, 50, 50);
 				yval += 60;
 			}
 			else {
 				GameObject.R.textSize(20);
+				GameObject.R.fill(255);
 				yval += 40;
 			}
 			
 			String str = (i+1) + ". " + score;
-			GameObject.R.text(str, Main.WIDTH*0.5F-100, yval);
+			GameObject.R.text(str, Main.WIDTH*0.5F + 200, yval);
 			
 			
 		}
